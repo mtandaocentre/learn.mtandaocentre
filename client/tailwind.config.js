537eada848b368a0ef781f18c60b21d366996a13
Mtandao Centre <mtandaocentre@gmail.com>
2025-05-20 11:23:28 +0300

    Configured tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: "#1a1a2e",
+          darker: "#16213e",
+          darkest: "#0f172a",
+        },
+        text: {
+          light: "#e6f1ff",
+          lighter: "#f8fafc",
+        },
+        accent: "#4f46e5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
