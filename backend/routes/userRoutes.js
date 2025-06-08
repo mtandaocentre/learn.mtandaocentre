d413baec89b39925f86bdf02924168c7fb749767
mtandao <mtandaocentre@gmail.com>
2025-06-08 08:56:43 +0300

    Created userRoute with get methods saved articles and reading progress

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+import {
+  getUserSavedArticles,
+  getUserReadingProgress,
+} from "../controllers/userController.js";
+import { protect } from "../middlewares/auth.js";
+
+// Protected routes (user must be logged in)
+router.get("/:userId/saved", protect, getUserSavedArticles);
+router.get("/:userId/progress", protect, getUserReadingProgress);
+
+export default router;
