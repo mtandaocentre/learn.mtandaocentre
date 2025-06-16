a218fc17616193512b2c26eeb6a452bb6aaf5f66
mtandao <mtandaocentre@gmail.com>
2025-06-17 01:02:10 +0300

    Added verify email route

--- client/src/AppContent.jsx
+++ client/src/AppContent.jsx
@@ -1,41 +1,33 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Chat from "./components/Chat";
 import Home from "./pages/Home";
 import Articles from "./pages/Articles";
 import ArticleDetail from "./pages/ArticleDetail";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import VerifyEmail from "./pages/VerifyEmail";
 import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
 
 const AppContent = () => {
-  const location = useLocation();
-  const hideChatPaths = ["/login", "/register", "/admin"];
-
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       
-      {/* Main content area - fixed to fill width */}
-      <main className="flex-grow w-full"> {/* Added w-full here */}
+      {/* Main content area */}
+      <main className="flex-grow w-full">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/articles/:id" element={<ArticleDetail />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </main>
-
-      {!hideChatPaths.includes(location.pathname) && (
-        <div className="fixed bottom-4 right-4 z-50 w-[350px]">
-          <Chat />
-        </div>
-      )}
 
       <Footer />
     </div>
